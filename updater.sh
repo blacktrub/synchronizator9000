c22e6994204d2bc65fed73fabd97f9b59aacf4bf
blacktrub <lightlocdog@gmail.com>
2020-12-27 20:05:13 +0300

    updater work only once per day

--- updater.sh
+++ updater.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd ~/.sync
-git pull
+touch ~/.sync/last_update
+current=$(date +"%D")
+lastupdate=$(cat ~/.sync/last_update)
+if [ "$current" = "$lastupdate" ]
+then
+    exit 0
+else
+    cd ~/.sync
+    git pull
+    echo $current > ~/.sync/last_update
+fi
 
