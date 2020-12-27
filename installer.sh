4c73d41b608b1117cee4e4c89478c0866ca4c011
blacktrub <lightlocdog@gmail.com>
2020-12-27 19:39:47 +0300

    autoenable vim config in installer

--- installer.sh
+++ installer.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
 mkdir -p .sync
 git clone git@github.com:blacktrub/vscode-config.git .sync
+
+touch ~/.vimrc
+if grep -q '.sync/.config/.vimrc' ~/.vimrc
+then
+    echo "vim pass enable"
+else
+    echo "try
+source ~/.sync/.config/.vimrc
+catch
+endtry" >> ~/.vimrc
+fi
